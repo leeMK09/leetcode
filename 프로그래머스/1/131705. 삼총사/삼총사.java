5cc0aca0ab86b780d6a99237a51f23dec5f00076
leeMK09 <68103540+leeMK09@users.noreply.github.com>
2025-09-27 13:28:55 +0900

    [level 1] Title: 삼총사, Time: 0.03 ms, Memory: 81.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/131705. 삼총사/삼총사.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int solution(int[] number) {
+        int count = 0;
+        int length = number.length;
+        
+        for (int i = 0; i < length - 2; i++) {
+            for (int j = i + 1; j < length - 1; j++) {
+                for (int k = j + 1; k < length; k++) {
+                    if (number[i] + number[j] + number[k] == 0) {
+                        count++;
+                    }
+                }
+            }
+        }
+        return count;
+    }
+}
